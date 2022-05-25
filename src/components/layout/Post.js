87d2e8e45a5ba6ff18eea25205035c0696e0fd84
Luciano <luciano.scp@hotmail.com>
2022-05-25 14:59:30 -0300

    Refactored

--- src/components/layout/Post.js
+++ src/components/layout/Post.js
@@ -1,22 +1,22 @@
 import { IconContext } from "react-icons";
 import { BsPersonFill } from "react-icons/bs";
-
+import { Row, Col } from "react-bootstrap";
 
 let Post = () => {
   return (
-    <div className="row border-top border-bottom">
-      <div className="col-2 text-center">
+    <Row className="border-top border-bottom">
+      <Col xs={2} className="text-center">
         <IconContext.Provider value={{ color: "black", size: "1.5em", title: "Avatar" }}>
           <BsPersonFill />
         </IconContext.Provider>
-      </div>
-      <div className="col-10">
+      </Col>
+      <Col xs={10}>
         <p><b>Nickname</b> <span className="secondary">- 1h</span></p>
         <p>Nullam a tincidunt dolor. Aenean et porta mauris, eget venenatis odio. Sed vel
           fermentum ipsum. Curabitur laoreet felis purus, quis efficitur felis semper a. Praesent orci aliquam.
         </p>
-      </div>
-    </div>
+      </Col>
+    </Row>
   );
 }
 
--- src/components/pages/Feed.js
+++ src/components/pages/Feed.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import { BsPlusLg } from "react-icons/bs";
 import { IconContext } from "react-icons";
 import NewPostModal from './../layout/NewPostModal';
-
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 let Feed = () => {
   const [modalShow, setModalShow] = useState(false);
@@ -12,26 +12,26 @@ let Feed = () => {
     <section id="feed">
       <NewPostModal show={modalShow} onHide={() => setModalShow(false)} />
       <h1 className="text-center">Micro Blog</h1>
-      <div className="container-fluid">
-        <div className="row">
-          <div id="person" className="col-2">
-            <div id="new-post" onClick={() => setModalShow(true)} className="rounded-circle bg-dark text-center mx-auto">
-              <IconContext.Provider value={{ color: "white", className: "new-post", size: "1.5em", title: "New post" }}>
+      <Container fluid>
+        <Row>
+          <Col xs={2} id="person">
+            <Button id="new-post" onClick={() => setModalShow(true)} className="rounded-circle bg-dark">
+              <IconContext.Provider value={{ color: "white", size: "1.5em", title: "New post" }}>
                 <BsPlusLg />
               </IconContext.Provider>
-            </div>
-          </div>
-          <div id="content" className="col-10">
-            <div className="row border">
-              <div className="col-12">
+            </Button>
+          </Col>
+          <Col xs={10} id="content">
+            <Row className="border">
+              <Col xs={12} className="col-12">
                 <Post />
                 <Post />
                 <Post />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
     </section>
   )
 }
