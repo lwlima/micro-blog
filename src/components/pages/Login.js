2336c92539d94c161fba660f298df96ddcc9eff6
Luciano <luciano.scp@hotmail.com>
2022-05-25 14:50:51 -0300

    Add page login

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,8 +1,30 @@
-let Login = () => {
-  return(
-    <div>
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
-    </div>
+let Login = () => {
+  return (
+    <section id="login">
+      <Container>
+        <Row>
+          <Col xs={9} className="mx-auto">
+            <Form>
+              <Form.Group className="mb-3" controlId="email">
+                <Form.Label>E-mail</Form.Label>
+                <Form.Control type="email" placeholder="Digite seu email" />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="password">
+                <Form.Label>Senha</Form.Label>
+                <Form.Control type="password" placeholder="Digite sua Senha" />
+              </Form.Group>
+
+              <Button variant="primary" type="submit">
+                Login
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   )
 }
 
