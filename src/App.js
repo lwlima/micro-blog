800bbf07a3c59dcce371f29f104832cb4259fdd7
Luciano <luciano.scp@hotmail.com>
2022-05-25 08:54:10 -0300

    Add layout footer

--- src/App.js
+++ src/App.js
@@ -90,6 +90,10 @@ function App() {
           </div>
         </div>
       </div>
+      <footer class="text-center">
+        <p>Teste prático - <a target="_blank" class="trapi" href="www.trapi.com.br">Trapi</a></p>
+        <p>Desenvolvido por <a target="_blank" href="www.lucianolima.dev">Luciano Lima</a></p>
+      </footer>
     </div>
   );
 }
