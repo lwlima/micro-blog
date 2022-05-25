24d29b1577f74d9fac45f1d782f649f12e3a8677
Luciano <luciano.scp@hotmail.com>
2022-05-25 13:42:29 -0300

    refactored newpostmodal

--- src/App.js
+++ src/App.js
@@ -1,43 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BsPlusLg, BsPersonFill } from "react-icons/bs";
 import { IconContext } from "react-icons";
-import Modal from 'react-bootstrap/Modal'
-import Button from 'react-bootstrap/Button'
 import { useState } from 'react';
-
-function NewPostModal(props) {
-  const [text, setText] = useState("");
-  const MAXCHAR = 200;
-
-  function handleKeyPress(event) {
-    if(text.length == MAXCHAR && event.key != "Backspace"){
-      event.preventDefault();
-    }
-  }
-
-  function handleChange(event){
-      setText(event.target.value);
-  }
-
-  return (
-    <Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Nova postagem
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <textarea id="text-post" onChange={handleChange} onKeyPress={handleKeyPress} value={text} class="col-12" rows="7" placeholder="O que está acontecendo?"></textarea>
-      </Modal.Body>
-      <Modal.Footer>
-            <p>{200-text.length} Caracteres restantes</p>
-        <Button>Postar</Button>
-      </Modal.Footer>
-    </Modal>
-  );
-}
+import NewPostModal from './components/layout/NewPostModal'
 
 function App() {
   const [modalShow, setModalShow] = useState(false);
---
+++ src/components/layout/NewPostModal.js
@@ -0,0 +1,36 @@
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
+import { useState } from 'react';
+
+let NewPostModal = props => {
+  const [text, setText] = useState("");
+  const MAXCHAR = 200;
+
+  let handleKeyPress = event => {
+    if (text.length == MAXCHAR && event.key != "Backspace")
+      event.preventDefault();
+  }
+
+  let handleChange = event => {
+    setText(event.target.value);
+  }
+
+  return (
+    <Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Nova postagem
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <textarea id="text-post" onChange={handleChange} onKeyPress={handleKeyPress} value={text} class="col-12" rows="7" placeholder="O que está acontecendo?"></textarea>
+      </Modal.Body>
+      <Modal.Footer>
+        <p>{200 - text.length} Caracteres restantes</p>
+        <Button>Postar</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default NewPostModal;
