82a757478e864ac8312d622fc4efd149f916cd4b
Luciano <luciano.scp@hotmail.com>
2022-05-24 22:42:02 -0300

    Add layout posts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BsPlusLg, BsPersonFill } from "react-icons/bs";
+import { IconContext } from "react-icons";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 class="text-center">Micro Blog</h1>
+      <div class="container-fluid">
+        <div class="row">
+          <div id="person" class="col-2">
+            <div id="new-post" class="rounded-circle bg-dark text-center">
+              <IconContext.Provider value={{ color: "white", className: "new-post", size: "1.5em", title: "New post" }}>
+                <BsPlusLg />
+              </IconContext.Provider>
+            </div>
+          </div>
+          <div id="content" class="col-10">
+            <div class="row border">
+              <div class="col-12">
+                <div class="row border-top border-bottom">
+                  <div class="col-2 text-center">
+                    <IconContext.Provider value={{ color: "black", className: "sla", size: "1.5em", title: "Avatar" }}>
+                      <BsPersonFill />
+                    </IconContext.Provider>
+                  </div>
+                  <div class="col-10">
+                    <p><b>Nickname</b> <span class="secondary">- 1h</span></p>
+                    <p>Nullam a tincidunt dolor. Aenean et porta mauris, eget venenatis odio. Sed vel
+                      fermentum ipsum. Curabitur laoreet felis purus, quis efficitur felis semper a. Praesent orci aliquam.
+                    </p>
+                  </div>
+                </div>
+                <div class="row border-top border-bottom">
+                  <div class="col-2 text-center">
+                    <IconContext.Provider value={{ color: "black", className: "sla", size: "1.5em", title: "Avatar" }}>
+                      <BsPersonFill />
+                    </IconContext.Provider>
+                  </div>
+                  <div class="col-10">
+                    <p><b>Nickname</b> <span class="secondary">- 1h</span></p>
+                    <p>Nullam a tincidunt dolor. Aenean et porta mauris, eget venenatis odio. Sed vel
+                      fermentum ipsum. Curabitur laoreet felis purus, quis efficitur felis semper a. Praesent orci aliquam.
+                    </p>
+                  </div>
+                </div>
+                <div class="row border-top border-bottom">
+                  <div class="col-2 text-center">
+                    <IconContext.Provider value={{ color: "black", className: "sla", size: "1.5em", title: "Avatar" }}>
+                      <BsPersonFill />
+                    </IconContext.Provider>
+                  </div>
+                  <div class="col-10">
+                    <p><b>Nickname</b> <span class="secondary">- 1h</span></p>
+                    <p>Nullam a tincidunt dolor. Aenean et porta mauris, eget venenatis odio. Sed vel
+                      fermentum ipsum. Curabitur laoreet felis purus, quis efficitur felis semper a. Praesent orci aliquam.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
