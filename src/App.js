d841ff9eeb51655fb0f2611071405b77b28a5c99
Luciano <luciano.scp@hotmail.com>
2022-05-25 11:19:04 -0300

    Add characters counter

--- src/App.js
+++ src/App.js
@@ -8,6 +8,20 @@ import Button from 'react-bootstrap/Button'
 import { useState } from 'react';
 
 function NewPostModal(props) {
+  const [text, setText] = useState("");
+  const MAXCHAR = 200;
+
+  function handleKeyPress(event) {
+    if(text.length == MAXCHAR && event.key != "Backspace"){
+      event.preventDefault();
+    }
+  }
+
+  function handleChange(event){
+      setText(event.target.value);
+      console.log(text.length);
+  }
+
   return (
     <Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
       <Modal.Header closeButton>
@@ -16,9 +30,10 @@ function NewPostModal(props) {
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <textarea class="col-12" rows="7" placeholder="O que está acontecendo?"></textarea>
+        <textarea id="text-post" onChange={handleChange} onKeyPress={handleKeyPress} value={text} class="col-12" rows="7" placeholder="O que está acontecendo?"></textarea>
       </Modal.Body>
       <Modal.Footer>
+            <p>{200-text.length} Caracteres restantes</p>
         <Button>Postar</Button>
       </Modal.Footer>
     </Modal>
