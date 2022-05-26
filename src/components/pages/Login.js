790619781bbc60bd81216fb3af67f61599e5c8d8
Luciano <luciano.scp@hotmail.com>
2022-05-26 08:35:29 -0300

    Import Alert

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,4 +1,4 @@
-import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import { Container, Row, Col, Form, Button, Alert } from "react-bootstrap";
 import { useState } from 'react';
 
 let Login = () => {
